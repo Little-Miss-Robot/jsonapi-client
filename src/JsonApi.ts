ddd0b487333e5bf59d35d6325be264c947a717b7
Rein Van Oyen <rein.vanoyen@littlemissrobot.com>
2025-03-13 16:34:21 +0100

    Update JsonApi.ts

--- src/JsonApi.ts
+++ src/JsonApi.ts
@@ -7,6 +7,8 @@ export default class JsonApi {
 
 	public static init(config: TConfigAttributes) {
 
+		console.log('Initializing JSON:API CLient');
+
 		Config.setAll(config);
 
 		Container.singleton('ClientInterface', () => {
